e43472d7e0c67d59ae8b8f0054810571ea743cfb
Matt Walker <walkermatt@longwayaround.org.uk>
2015-03-12 14:30:43 +0000

    Force users to change password every x days

---
+++ Scripts/resetpasswd/setup.sql
@@ -0,0 +1,40 @@
+-- Ensure the GeoNetwork users table is audited
+SELECT audit.audit_table('public.users');
+
+-- Define a trigger to ensure a user has the highest profile assigned when they
+-- change their password from the default reset password
+
+CREATE OR REPLACE FUNCTION public.users_password_change_update_profile() RETURNS TRIGGER AS $function$
+BEGIN
+IF OLD.password = rpad('', 80, 'a') AND NEW.password <> rpad('', 80, 'a') THEN
+    NEW.profile := (SELECT profile
+                    FROM
+                        (SELECT event_id,
+                            row_data,
+                            changed_fields,
+                            CASE
+                                WHEN changed_fields ? 'password'
+                                        AND changed_fields->'password' = rpad('', 80, 'a') THEN row_data->'profile'
+                                ELSE changed_fields->'profile'
+                            END AS profile
+                        FROM audit.logged_actions
+                        WHERE SCHEMA_NAME = 'public'
+                            AND TABLE_NAME = 'users'
+                            AND changed_fields ? 'profile'
+                            AND (row_data->'id')::int = NEW.id
+                        ORDER BY event_id) AS a LIMIT 1);
+END IF;
+RETURN NEW;
+END;
+$function$ LANGUAGE plpgsql;
+COMMENT ON FUNCTION public.users_password_change_update_profile() IS $comment$
+Trigger function to ensure that if a users password is being changed from the
+default reset password then ensure the users profile is updated to the highest
+profile that the user has assigned in usergroups
+$comment$;
+
+DROP TRIGGER IF EXISTS users_password_change ON users;
+CREATE TRIGGER users_password_change
+BEFORE UPDATE OF password ON public.users
+FOR EACH ROW
+EXECUTE PROCEDURE public.users_password_change_update_profile();
