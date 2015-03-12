da2384c6d4906268b839a8f61e22c6bb8e3652e6
Matt Walker <walkermatt@longwayaround.org.uk>
2015-03-12 14:30:43 +0000

    Force users to change password every x days

---
+++ Scripts/resetpasswd/resetpasswd.sql
@@ -0,0 +1,24 @@
+-- Reset the password and update profile to RegisteredUser for all users who
+-- have not changed their password in the last x days and return their rows
+-- from the users table
+WITH need_reset AS
+    (SELECT id
+     FROM public.users
+     WHERE id NOT IN
+             (SELECT DISTINCT (row_data->'id')::int AS userid
+              FROM audit.logged_actions
+              WHERE SCHEMA_NAME = 'public'
+                  AND TABLE_NAME = 'users'
+                  AND ((action = 'U'
+                        AND changed_fields ? 'password'
+                        AND action_tstamp_tx > CURRENT_TIMESTAMP - INTERVAL '%(num_days)s' DAY)
+                       OR (action = 'I'))
+              ORDER BY userid)
+     ORDER BY id),
+disabled AS
+    (UPDATE public.users
+        SET profile = 'RegisteredUser', password = rpad('', 80, 'a')
+        WHERE id IN
+                (SELECT id
+                FROM need_reset) RETURNING *)
+SELECT * FROM disabled;
