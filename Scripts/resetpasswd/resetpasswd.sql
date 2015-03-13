3e871afb22eeb195537016c09d285c95229f077c
Matt Walker <walkermatt@longwayaround.org.uk>
2015-03-13 13:19:57 +0000

    Ensure new users who have not changed password are picked up
    Previously if a new user was added and their password was not changed
    then they would not be identified as having to change their password
    after `num_days` has elapsed as the predicate for inserted rows did not
    take into account the timestamp.

--- Scripts/resetpasswd/resetpasswd.sql
+++ Scripts/resetpasswd/resetpasswd.sql
@@ -6,14 +6,12 @@ WITH need_reset AS
      FROM public.users
      WHERE id NOT IN
              (SELECT DISTINCT (row_data->'id')::int AS userid
-              FROM audit.logged_actions
-              WHERE SCHEMA_NAME = 'public'
-                  AND TABLE_NAME = 'users'
-                  AND ((action = 'U'
-                        AND changed_fields ? 'password'
-                        AND action_tstamp_tx > CURRENT_TIMESTAMP - INTERVAL '%(num_days)s' DAY)
-                       OR (action = 'I'))
-              ORDER BY userid)
+                FROM audit.logged_actions
+                WHERE SCHEMA_NAME = 'public'
+                    AND TABLE_NAME = 'users'
+                    AND action_tstamp_tx > CURRENT_TIMESTAMP - INTERVAL '%(num_days)s' DAY
+                    AND ((action = 'U' AND changed_fields ? 'password') OR action = 'I')
+                ORDER BY userid)
      ORDER BY id),
 disabled AS
     (UPDATE public.users
