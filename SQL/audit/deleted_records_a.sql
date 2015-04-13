6beffa105b7401d85b17bb80c506cc6cc14d2404
archaeogeek <jo.k.cook@gmail.com>
2015-04-13 11:15:11 +0100

    Added custom SQL for reporting interface

---
+++ SQL/audit/deleted_records_a.sql
@@ -0,0 +1,18 @@
+﻿-- View: audit.deleted_records_a
+
+-- DROP VIEW audit.deleted_records_a;
+
+CREATE OR REPLACE VIEW audit.deleted_records_a AS 
+ SELECT logged_actions.client_query,
+    ('0'::text || COALESCE(logged_actions.row_data -> 'id'::text, '0'::text))::integer AS uuid,
+    XMLPARSE(DOCUMENT logged_actions.row_data -> 'data'::text STRIP WHITESPACE) AS data_xml,
+    logged_actions.action,
+    logged_actions.action_tstamp_tx,
+    logged_actions.row_data -> 'owner'::text AS owner,
+    ('0'::text || COALESCE(logged_actions.row_data -> 'groupowner'::text, '0'::text))::integer AS group_owner
+   FROM audit.logged_actions
+  WHERE logged_actions.table_name = 'metadata'::text AND logged_actions.action = 'D'::text AND logged_actions.action_tstamp_tx > ('now'::text::date - '7 days'::interval)
+  ORDER BY ('0'::text || COALESCE(logged_actions.row_data -> 'id'::text, '0'::text))::integer;
+
+ALTER TABLE audit.deleted_records_a
+  OWNER TO geonetwork;
---
+++ SQL/public/records_date_updated.sql
@@ -0,0 +1,16 @@
+﻿-- View: records_date_updated
+
+-- DROP VIEW records_date_updated;
+
+CREATE OR REPLACE VIEW records_date_updated AS 
+ SELECT metadata_xml.id,
+    metadata_xml.changedate,
+    ('"'::text || array_to_string(xpath('(/gmd:MD_Metadata/gmd:identificationInfo/gmd:MD_DataIdentification/gmd:pointOfContact[gmd:CI_ResponsibleParty/gmd:role/gmd:CI_RoleCode="custodian"]/gmd:CI_ResponsibleParty/gmd:individualName/gco:CharacterString/text())[1]'::text, metadata_xml.data_xml, '{{gco,http://www.isotc211.org/2005/gco},{gmd,http://www.isotc211.org/2005/gmd}}'::text[]), '-'::text)) || '"'::text AS custodian,
+    ('"'::text || array_to_string(xpath('/gmd:MD_Metadata/gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString/text()'::text, metadata_xml.data_xml, '{{gco,http://www.isotc211.org/2005/gco},{gmd,http://www.isotc211.org/2005/gmd}}'::text[]), '-'::text)) || '"'::text AS title,
+    groups.name
+   FROM metadata_xml
+     LEFT JOIN groups ON metadata_xml.groupowner = groups.id
+  WHERE to_date(metadata_xml.changedate::text, 'YYYY-MM-DD'::text) < ('now'::text::date - '3 mons'::interval);
+
+ALTER TABLE records_date_updated
+  OWNER TO geonetwork;
