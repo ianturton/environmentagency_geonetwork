c5ce064906111d3ea84fc9d72233d68df20d02a5
archaeogeek <jo.k.cook@gmail.com>
2015-09-10 10:41:39 +0100

    Modified SQL for records status report

--- SQL/public/records_status.sql
+++ SQL/public/records_status.sql
@@ -1,19 +1,10 @@
-﻿-- View: records_status
-
--- DROP VIEW records_status;
-
-CREATE OR REPLACE VIEW records_status AS 
- SELECT metadata_xml.id,
-    statusvalues.name,
-    metadatastatus.changedate,
-    metadatastatus.changemessage,
-    ('"'::text || array_to_string(xpath('/gmd:MD_Metadata/gmd:distributionInfo/gmd:MD_Distribution/gmd:transferOptions/gmd:MD_DigitalTransferOptions/gmd:onLine/gmd:CI_OnlineResource/gmd:linkage/gmd:URL/text()'::text, metadata_xml.data_xml, '{{gco,http://www.isotc211.org/2005/gco},{gmd,http://www.isotc211.org/2005/gmd}}'::text[]), '-'::text)) || '"'::text AS orl,
-    ('"'::text || array_to_string(xpath('/gmd:MD_Metadata/gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString/text()'::text, metadata_xml.data_xml, '{{gco,http://www.isotc211.org/2005/gco},{gmd,http://www.isotc211.org/2005/gmd}}'::text[]), '-'::text)) || '"'::text AS title
-   FROM metadata_xml
-     LEFT JOIN metadatastatus ON metadatastatus.metadataid = metadata_xml.id
-     LEFT JOIN statusvalues ON metadatastatus.statusid = statusvalues.id
-  WHERE metadatastatus.changedate IS NULL OR metadatastatus.changedate::text = (( SELECT max(metadatastatus_1.changedate::text) AS max
-           FROM metadatastatus metadatastatus_1));
-
-ALTER TABLE records_status
-  OWNER TO geonetwork;
+﻿select metadata_xml.id, metadata_xml.uuid, 
+(array_to_string(xpath('/gmd:MD_Metadata/gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString/text()'::text, metadata_xml.data_xml, '{{gco,http://www.isotc211.org/2005/gco},{gmd,http://www.isotc211.org/2005/gmd}}'::text[]), '-'::text)) ::text AS title, 
+name as status, 
+userid, 
+foo.changedate, 
+changemessage from 
+(SELECT DISTINCT ON (metadataid) * FROM metadatastatus ORDER BY metadataid, changedate desc) foo 
+left join statusvalues on foo.statusid= statusvalues.id
+right join  metadata_xml on foo.metadataid = metadata_xml.id
+order by metadata_xml.id
